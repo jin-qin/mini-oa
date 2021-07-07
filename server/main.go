46d8ce10561cd22b7245c4ee4151b52230c0db57
Jin Qin <qin_jin@live.com>
2021-07-06 19:55:19 -0700

    add initial users logic

--- server/main.go
+++ server/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"mini-oa-server/common/util/config"
+	"mini-oa-server/service/users"
 
 	"github.com/gin-gonic/gin"
 )
@@ -15,6 +16,10 @@ func main() {
 
 	// Set a lower memory limit for multipart forms (default is 32 MiB)
 	r.MaxMultipartMemory = 8 << 20 // 8 MiB
+
+	v1 := r.Group("/v1")
+	users_grp := v1.Group("/users")
+	users.RegisterUserRouters(users_grp)
 
 	r.Run(fmt.Sprintf(":%d", appConfig.ServerPort))
 }
--- server/service/users/routers.go
+++ server/service/users/routers.go
@@ -1,1 +1,22 @@
 package users
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func RegisterUserRouters(r *gin.RouterGroup) {
+	r.POST("/register", userRegisterRoute)
+	r.POST("/login", userLoginRoute)
+}
+
+func userLoginRoute(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "login",
+	})
+}
+
+func userRegisterRoute(c *gin.Context) {
+	c.JSON(201, gin.H{
+		"message": "register",
+	})
+}
